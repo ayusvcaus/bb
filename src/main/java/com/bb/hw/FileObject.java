2ff940aca6b176fc22360ea2669e28e409b1abdf
ayusvcaus <ayucaus@yahoo.com>
2020-04-02 16:36:30 -0700

    Adjust indention

--- src/main/java/com/bb/hw/FileObject.java
+++ src/main/java/com/bb/hw/FileObject.java
@@ -17,46 +17,44 @@ public class FileObject implements Comparable<FileObject> {
 	private char concernedChar;
 	private int count;
 	
-	public FileObject(String fileName, String content, char concernedChar) {
-		this.fileName=fileName;
-		this.concernedChar=concernedChar;
-		
-		countConcernedChar(content);
-	}
+    public FileObject(String fileName, String content, char concernedChar) {
+        this.fileName=fileName;
+        this.concernedChar=concernedChar;	
+        countConcernedChar(content);
+    }
 	
-	public String getFileName() {
-		return fileName;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 	
-	public char getConcernChar() {
-		return concernedChar;
-	}
+    public char getConcernChar() {
+        return concernedChar;
+    }
 
-
-	public int getCount() {
-		return count;
-	}
+    public int getCount() {
+        return count;
+    }
 	
 	/*
 	 * Get count of the concerned character
 	 */	
-	private void countConcernedChar(String content) {
-		if (content!=null) {
-			for (char c : content.toCharArray()) {
-				if (c==concernedChar) {
-					count++;
-				}
-			}
-		}
-		s_logger.debug("File name: "+fileName+", concerned char: '"+concernedChar+"',  count: "+count);
-	}
+    private void countConcernedChar(String content) {
+        if (content!=null) {
+            for (char c : content.toCharArray()) {
+                if (c==concernedChar) {
+                    count++;
+                }
+            }
+        }
+        s_logger.debug("File name: "+fileName+", concerned char: '"+concernedChar+"',  count: "+count);
+    }
 	
 	/*
 	 * Compare 2 objects on the counts, then on the file names in case the counts are the same
 	 */
-	@Override
-	public int compareTo(FileObject fe) {
-		int diff=this.count-fe.getCount();
-	    return diff==0 ? this.fileName.compareTo(fe.getFileName()) : diff;
-	}
+    @Override
+    public int compareTo(FileObject fe) {
+        int diff=this.count-fe.getCount();
+        return diff==0 ? this.fileName.compareTo(fe.getFileName()) : diff;
+    }
 }

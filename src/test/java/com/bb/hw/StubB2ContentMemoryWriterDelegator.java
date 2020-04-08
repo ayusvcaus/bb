0b72533ffc7e782d303fc101d0846cad0d0e9b3c
ayusvcaus <ayucaus@yahoo.com>
2020-04-08 02:26:49 -0700

    Fix indentation

--- src/test/java/com/bb/hw/StubB2ContentMemoryWriterDelegator.java
+++ src/test/java/com/bb/hw/StubB2ContentMemoryWriterDelegator.java
@@ -1,31 +1,31 @@
 package com.bb.hw;
 
-/** 
-* This class is for unit test only, the getData() is overridden
-* 
-* @author : ayu
-* @date creation time：Mar 31, 2020 21:15:15 PM 
-* @version 1.0 
-*/
+/**
+ * This class is for unit test only, the getData() is overridden
+ * 
+ * @author : ayu
+ * @date creation time：Mar 31, 2020 21:15:15 PM
+ * @version 1.0
+ */
 
 public class StubB2ContentMemoryWriterDelegator extends B2ContentMemoryWriterDelegator {
 
     private byte[] data;
-    
+
     public StubB2ContentMemoryWriterDelegator() {
-    	super(false);
+        super(false);
     }
-    
+
     public void setData(String s) {
-    	if (s!=null) {
-    	    data=s.getBytes();
-    	} else {
-    		data=new byte[0];
-    	}
+        if (s != null) {
+            data = s.getBytes();
+        } else {
+            data = new byte[0];
+        }
     }
-    
+
     @Override
     public byte[] getData() {
-    	return data;
+        return data;
     }
 }
